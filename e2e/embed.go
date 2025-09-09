b61bdc65670e393f765d9abd3d6115130fa27062
Ramiz Polic <rpolic@cisco.com>
2025-09-09 15:41:34 +0200

    feat: add validation support for all supported schemas
    Signed-off-by: Ramiz Polic <rpolic@cisco.com>

--- e2e/embed.go
+++ e2e/embed.go
@@ -5,6 +5,9 @@ package e2e
 
 import _ "embed"
 
+//go:embed fixtures/valid_v0.3.1_record.json
+var validV031Record []byte
+
 //go:embed fixtures/valid_v0.7.0_record.json
 var validV070Record []byte
 
